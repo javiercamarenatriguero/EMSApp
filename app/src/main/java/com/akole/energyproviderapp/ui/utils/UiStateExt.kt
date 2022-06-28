127295c672a9fc987aae17d28d19d7fa7f2ebe19
Javier Camarena <javier.camtri@gmail.com>
2022-06-29 00:43:06 +0200

    Refactor Percentage operations

--- app/src/main/java/com/akole/energyproviderapp/ui/utils/UiStateExt.kt
+++ app/src/main/java/com/akole/energyproviderapp/ui/utils/UiStateExt.kt
@@ -5,16 +5,30 @@ import com.akole.energyproviderapp.ui.screens.home.UiState
 internal fun UiState.getGridPowerPercentageToBuilding() : Float {
     // According to the API, the car is charged JUST with grid power
     val powerToBuildingFromGrid = if (quasarsPower > 0) gridPower - quasarsPower else gridPower
-    val result = 100 * powerToBuildingFromGrid / buildingDemandPower
-    return if (result.equals(Float.NaN)) 0.0f else result
+    return getPercentage(
+        sourcePower = powerToBuildingFromGrid,
+        totalPower = buildingDemandPower
+    )
 }
 
 internal fun UiState.getSolarPowerPercentageToBuilding() : Float {
-    val result = 100 * (buildingDemandPower + quasarsPower - gridPower) / buildingDemandPower
-    return if (result.equals(Float.NaN)) 0.0f else result
+    val powerToBuildingFromSolar = buildingDemandPower + quasarsPower - gridPower
+    return getPercentage(
+        sourcePower = powerToBuildingFromSolar,
+        totalPower = buildingDemandPower
+    )
 }
 
 internal fun UiState.getCarElectricPowerPercentageToBuilding() : Float {
-    val result = if (quasarsPower < 0) 100 * kotlin.math.abs(quasarsPower) / buildingDemandPower else 0.0f
+    val powerToBuildingFromElectricCar = if (quasarsPower < 0) kotlin.math.abs(quasarsPower) else 0.0f
+    return getPercentage(
+        sourcePower = powerToBuildingFromElectricCar,
+        totalPower = buildingDemandPower
+    )
+}
+
+private fun getPercentage(sourcePower: Float, totalPower: Float): Float {
+    val result = 100 * sourcePower / totalPower
     return if (result.equals(Float.NaN)) 0.0f else result
 }
+
